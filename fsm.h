4a579bdb66afedc9ea959db269e0c76352e0c498
lauritsriple <laurits.triple@gmail.com>
2015-02-11 16:39:53 +0100

    changed prototype of function

--- fsm.h
+++ fsm.h
@@ -3,7 +3,7 @@ ifndef FSM_H
 
 void fsm_init();
 void fsm_evStopPressed();
-void fsm_evButtonUpDownPressed(int floor, int directio);
+void fsm_evButtonUpDownPressed(int floor, elev_button_type_t button);
 void fsm_evButtonOrderFloorPressed(int floor);
 void fsm_evIsFloor(int currentFloot);
 void fsm_evIsTimeout();
