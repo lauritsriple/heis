aa6bbd2da08f394f6ddb5a3cce95601e734c069e
proggemalla <malin_hh@hotmail.com>
2015-02-11 17:53:18 +0100

    lalalal

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
-#include <time.h>
+#include "heisdriver/elev.h"
+#include "fsm.h"
+
+
+int main (void) {
+	fsm_init()
+	while(true){
+		if (elev_get_floor_sensor_signal()!=-1){
+			fsm_isFloor(elev_get_floor_sensor_signal());
+		}
+
+		for (int i = 0; i<3; i++){ //Button types
+			for (int j = 0; j < 4; j++){
+				elev_get_button_signal((elev_button_type_t)i,j);
+			}
+		}
+}
+
+
+}
